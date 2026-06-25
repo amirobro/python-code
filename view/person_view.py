763e1d5e8cf84ed59f0179ef7a4f87563f4af0ef
amirobro <amirmmk13@gmail.com>
2026-06-25 17:30:32 +0330

    Update person_view.py

--- view/person_view.py
+++ view/person_view.py
@@ -1,88 +1,108 @@
 from tkinter import *
 import tkinter.ttk as ttk
-from model.db.persondb import *
 import tkinter.messagebox as msg
+
+from model.db.persondb import *
 from cotrooler.person_cotroller import *
 
-
 def reset_form():
-    name.set("")
-    family.set("")
-    national_id.set("")
+id.set(0)
+name.set("")
+family.set("")
+national_id.set("")
 
-    # table clean
-    for i in table.get_children():
-        table.delete(i)
+```
+for i in table.get_children():
+    table.delete(i)
 
-    # insert   to     table
+try:
     for person in find_all():
         table.insert("", END, values=person)
-
+except:
+    pass
+```
 
 def table_clik(event):
-    index = table.focus()
-    item = table.item(index)
-    person = item['values']
-    id.set(person[0])
-    name.set(person[1])
-    family.set(person[2])
-    national_id.set(person[3])
+index = table.focus()
 
+```
+if not index:
+    return
+
+item = table.item(index)
+person = item['values']
+
+if len(person) < 4:
+    return
+
+id.set(person[0])
+name.set(person[1])
+family.set(person[2])
+national_id.set(person[3])
+```
 
 def save_click():
-    status, data = save(name.get(), family.get(), national_id.get())
-    if status:
-        msg.showinfo("Save", data)
-        reset_form()
-    else:
-        msg.showerror("Save Error", data)
+status, data = save(name.get(), family.get(), national_id.get())
 
+```
+if status:
+    msg.showinfo("Save", data)
+    reset_form()
+else:
+    msg.showerror("Save Error", data)
+```
 
 def edit_click():
-    status, data = edit(id.get(), name.get(), family.get(), national_id.get())
-    if status:
-        msg.showinfo("Edit", "person edited")
-        reset_form()
-    else:
-        msg.showerror("Edit Error", data)
+status, data = edit(
+id.get(),
+name.get(),
+family.get(),
+national_id.get()
+)
 
+```
+if status:
+    msg.showinfo("Edit", "person edited")
+    reset_form()
+else:
+    msg.showerror("Edit Error", data)
+```
 
 def remove_click():
-    if msg.askyesno("Remove", "are u sure?"):
+if msg.askyesno("Remove", "are u sure?"):
 
-        status, data = remove(id.get())
-        if status:
-         msg.showinfo("remove", data)
-         reset_form()
-        else:
-            msg.showerror('remove,Error', data)
+```
+    status, data = remove(id.get())
 
+    if status:
+        msg.showinfo("Remove", data)
+        reset_form()
+    else:
+        msg.showerror("Remove Error", data)
+```
 
 win = Tk()
 win.title('Person info')
 win.geometry("800x600")
 
-# id
 id = IntVar()
 Label(win, text="ID").place(x=20, y=20)
 Entry(win, textvariable=id, state="readonly").place(x=90, y=20)
 
-# Name
 name = StringVar()
-Label(win, text="name").place(x=20, y=60)
+Label(win, text="Name").place(x=20, y=60)
 Entry(win, textvariable=name).place(x=90, y=60)
 
-# family
 family = StringVar()
-Label(win, text="family").place(x=20, y=100)
+Label(win, text="Family").place(x=20, y=100)
 Entry(win, textvariable=family).place(x=90, y=100)
 
-# national ID
 national_id = StringVar()
 Label(win, text="National ID").place(x=20, y=140)
 Entry(win, textvariable=national_id).place(x=90, y=140)
 
 table = ttk.Treeview(win, columns=(1, 2, 3, 4), show="headings")
+
 table.column(1, width=60)
 table.column(2, width=100)
 table.column(3, width=100)
@@ -90,7 +110,7 @@ table.column(4, width=100)
 
 table.heading(1, text="ID")
 table.heading(2, text="Name")
-table.heading(3, text="family")
+table.heading(3, text="Family")
 table.heading(4, text="National ID")
 
 table.bind("<ButtonRelease>", table_clik)
@@ -98,10 +118,13 @@ table.bind("<KeyRelease>", table_clik)
 
 table.place(x=300, y=20)
 
-Button(win, text="save", command=save_click, width=12).place(x=90, y=259)
-Button(win, text="edit", command=edit_click, width=12).place(x=190, y=259)
-Button(win, text="remove", command=remove_click, width=12).place(x=290, y=259)
+Button(win, text="Save", command=save_click, width=12).place(x=90, y=259)
+Button(win, text="Edit", command=edit_click, width=12).place(x=190, y=259)
+Button(win, text="Remove", command=remove_click, width=12).place(x=290, y=259)
 
 reset_form()
 
 win.mainloop()
+
+```
+```
